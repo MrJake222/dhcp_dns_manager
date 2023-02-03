38492173c09698180634ba37dc0d9c6df49847ee
MrJake222 <norbertzpilicy@gmail.com>
2023-02-03 19:50:43 +0100

    gen_config fixed

--- examples/gen_config.sh
+++ examples/gen_config.sh
@@ -19,10 +19,12 @@ v4_PREFIX="192.168"
 v4_EXTERNAL="8.8.8.8"
 v6_PREFIX="fd00"
 function call_gen {
-	./${GENERATOR} ${HOSTS} ${FIREWALL} ${v4_PREFIX} ${v4_EXTERNAL} ${v6_PREFIX} $1
+	${GENERATOR} ${HOSTS} ${FIREWALL} ${v4_PREFIX} ${v4_EXTERNAL} ${v6_PREFIX} $1
 }
 
 function dns {
+	echo "generating dns"
+	
 	serial=$(date +"%y%m%d%H%M")
 	cat ${BASE_DNS} | sed "s/SERIAL/${serial} ; serial/g" > ${OUT_DNS_INT}
 	cat ${BASE_DNS} | sed "s/SERIAL/${serial} ; serial/g" > ${OUT_DNS_EXT}
@@ -31,21 +33,29 @@ function dns {
 }
 
 function dhcp {
+	echo "generating dhcp"
+	
 	cp ${BASE_DHCP} ${OUT_DHCP}
 	call_gen dhcp >> ${OUT_DHCP}
 }
 
 function dhcpv6 {
+	echo "generating dhcpv6"
+	
 	cp ${BASE_DHCPv6} ${OUT_DHCPv6}
 	call_gen dhcpv6 >> ${OUT_DHCPv6}
 }
 
 function iptables {
 	# TODO
+	echo "generating iptables"
+	echo "unimplemented"
 }
 
 function iptables6 {
 	# TODO
+	echo "generating iptables6"
+	echo "unimplemented"
 }
 
 dns
