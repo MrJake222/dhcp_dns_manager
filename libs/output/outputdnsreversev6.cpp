f6cda64d72776d6b2c60b22a606376eccc12c51b
MrJake222 <norbertzpilicy@gmail.com>
2023-02-12 14:02:46 +0100

    ptr record no dot

--- libs/output/outputdnsreversev6.cpp
+++ libs/output/outputdnsreversev6.cpp
@@ -8,6 +8,6 @@ void OutputDNSReversev6::write(std::ostream& os, MachinePtrConst m, const Machin
     if (m->has_flag("nov6"))
         return;
 
-    os << m->get_ipv6().get_reverse_dns(0) << ".        PTR        ";
+    os << m->get_ipv6().get_reverse_dns(0) << "        PTR        ";
     os << m->get_fqdn() << "." << std::endl;
 }
