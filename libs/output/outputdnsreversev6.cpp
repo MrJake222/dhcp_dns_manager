bf69b94de9c44583ef9bb047dd6a8a58bfbef611
MrJake222 <norbertzpilicy@gmail.com>
2023-02-12 12:40:07 +0100

    rev6 fixed nov6 flag

--- libs/output/outputdnsreversev6.cpp
+++ libs/output/outputdnsreversev6.cpp
@@ -5,6 +5,9 @@
 void OutputDNSReversev6::write(std::ostream& os, MachinePtrConst m, const MachineVect& mvect) const {
     // 1.0.0.0.1.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0         PTR             c1.k21a.mrwski.eu.
 
+    if (m->has_flag("nov6"))
+        return;
+
     os << m->get_ipv6().get_reverse_dns(0) << ".        PTR        ";
     os << m->get_fqdn() << "." << std::endl;
 }
