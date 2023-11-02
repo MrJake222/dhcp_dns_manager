dd04345b8aef2d6bcc22b32b987f0057f2a0e6b2
MrJake222 <norbertzpilicy@gmail.com>
2023-11-02 21:26:10 +0100

    Changed nft chain names to uppercase
    Compliant with docker

--- libs/output/outputnftables.cpp
+++ libs/output/outputnftables.cpp
@@ -2,6 +2,10 @@
 
 #include "../util.hpp"
 
+// TODO export this as configuration
+const std::string FILTER_FORWARD = "inet filter FORWARD";
+const std::string NAT_PREROUTING = "ip nat PREROUTING";
+
 void OutputNftables::write(std::ostream& os, MachinePtrConst m, const MachineVect& mvect) const {
 
     for (const FirewallRule &rule: m->get_firewall_rules()) {
@@ -9,17 +13,17 @@ void OutputNftables::write(std::ostream& os, MachinePtrConst m, const MachineVec
         if (!m->has_flag("nov6")) {
             // ipv6 forward rule
             // add rule inet filter forward ip6 daddr 2001:470:6166::1:1 tcp dport 22 accept
-            os << "add rule inet filter forward ip6 daddr " << m->get_ipv6() << " " << rule.get_protocol() << " dport " << rule.get_internal_port() << " accept" << std::endl;
+            os << "add rule " << FILTER_FORWARD << " ip6 daddr " << m->get_ipv6() << " " << rule.get_protocol() << " dport " << rule.get_internal_port() << " accept" << std::endl;
         }
 
         // ipv4 forward rule
         // add rule inet filter forward ip daddr 172.16.1.1 tcp dport 22 accept
-        os << "add rule inet filter forward ip daddr " << m->get_ipv4() << " " << rule.get_protocol() << " dport " << rule.get_internal_port() << " accept" << std::endl;
+        os << "add rule " << FILTER_FORWARD << " ip daddr " << m->get_ipv4() << " " << rule.get_protocol() << " dport " << rule.get_internal_port() << " accept" << std::endl;
 
         // ipv4 nat
         // add rule ip nat prerouting ip saddr != $INTERNAL tcp dport 222 dnat to 172.16.1.1:22
         if (rule.should_pass_v4_external()) {
-            os << "add rule ip nat prerouting ip saddr != $INTERNAL " << rule.get_protocol() << " dport " << rule.get_external_port() << " dnat to " << m->get_ipv4() << ":" << rule.get_internal_port() << std::endl;
+            os << "add rule " << NAT_PREROUTING << " ip saddr != $INTERNAL " << rule.get_protocol() << " dport " << rule.get_external_port() << " dnat to " << m->get_ipv4() << ":" << rule.get_internal_port() << std::endl;
         }
     }
 }
